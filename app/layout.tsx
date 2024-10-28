cfa383e2a260f39da78b0e2a1654fa1dd2f1887d
Suyash Labde <suyashlabde123@gmail.com>
2024-10-28 09:54:03 +0530

    added sidebar and removed login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const geistSans = localFont({
@@ -26,14 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+   
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {/* <AppSidebar /> */}
+          {children}
+          <Footer />
+        </body>
+      </html>
+    
   );
 }
