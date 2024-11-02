98fcf31f7c43d9daea3cee4ede9fa0ce16bfe7ea
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-11-02 21:54:12 +0530

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,14 +1,12 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  // If you have any additional custom props, define them here
-  // e.g., customProp?: string;
-}
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "text", ...props }, ref) => {
+  ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
@@ -19,9 +17,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    );
+    )
   }
-);
-Input.displayName = "Input";
+)
+Input.displayName = "Input"
 
-export { Input };
+export { Input }
